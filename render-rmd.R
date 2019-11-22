b50c70f9f7fd564edd83d89f413dad9868318ec3
Erwin <lasrubieras@MacBook-Pro-4.local>
2019-11-22 11:29:38 -0600

    using if and for looks to automate the report creation

--- render-rmd.R
+++ render-rmd.R
@@ -1,6 +1,22 @@
 library(rmarkdown)
+library(stringr)
 file_names <- commandArgs(trailingOnly = TRUE)
 
-render("surveys-report.Rmd", 
+if("-a" %in% file_names){
+  folder <- file_names[2]
+  file_names <- Sys.glob(str_c(folder, '/*.csv'))
+} else {file_names <- file_names}
+
+
+for(file in file_names)
+{
+  out_name <- str_split_fixed( string = file,
+                         pattern = "[/|.]",
+                         n =3)
+  out_name <- str_c(out_name[2],'.html')
+  print(out_name)
+  render("surveys-report.Rmd", 
        output_dir = "reports",
-       params = list( file = file_names))
+       output_file = out_name,
+       params = list( file = file))
+}
